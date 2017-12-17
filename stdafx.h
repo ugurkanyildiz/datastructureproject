6e470723bb45a2ad1245bda0a97e82a93b82718d
ugurkanyildiz <34451825+ugurkanyildiz@users.noreply.github.com>
2017-12-17 08:14:23 +0300

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include<ctime>
+
+using namespace std;
+
+// TODO: reference additional headers your program requires here
